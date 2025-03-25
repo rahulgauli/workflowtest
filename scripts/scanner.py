596e922e06b700e8660b51dc731a52c34d22d2b7
rahulgauli <63265876+rahulgauli@users.noreply.github.com>
2025-03-25 14:27:15 -0500

    Update scanner.py

--- scripts/scanner.py
+++ scripts/scanner.py
@@ -1,2 +1,43 @@
-print("I am the script that will be running that triggers the scan")
-print("here are your vulnerability findings as JSON")
+import random 
+
+mock_denial_snyk_scan_response = {
+    "findings":
+        {
+            "critical_findings": {
+                "count": 5,
+                "types_findings": ["CVE-Random","CVE-Random2","CVE-2021-1234", "CVE-2021-1235"]
+                },
+            "high_findings": {
+                "count": 1,
+                "types_findings": ["CVE-2021-1236"]
+                },
+            "medium_findings": {
+                "count": 0,
+                "types_findings": []
+                },
+            }
+        }
+
+mock_approved_snyk_scan_response = {
+    "findings":
+        {
+            "critical_findings": {
+                "count": 0,
+                "types_findings": []
+                },
+            "high_findings": {
+                "count": 0,
+                "types_findings": []
+                },
+            "medium_findings": {
+                "count": 3,
+                "types_findings": ["CVE-2021-1237", "CVE-2021-1238", "CVE-2021-1239"]
+                },
+            }
+        }
+
+
+responses = [ mock_denial_snyk_scan_response, mock_approved_snyk_scan_response ]
+random_integer = random.randint(0,1)
+result = responses[random_integer]
+print(result)
